038382353d1387d76a59b2d83f7b5674b1705b46
Hidayatullah Wildan Ghaly Buchary <13521015@std.stei.itb.ac.id>
2024-06-03 19:51:15 +0700

    feat: add load game canvas script and integrate with UI, also addthe TODO for each function

--- Assets/Script/Menu/Main Menu/LoadGameCanvas.cs
+++ Assets/Script/Menu/Main Menu/LoadGameCanvas.cs
@@ -1,18 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LoadGameCanvas : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject _mainMenuCanvas;
+    [SerializeField] private GameObject _slotList;
+
+    private int _slotIDClicked = 0;
+    private Color _selectedColor = Color.blue;
+    private Color _defaultColor = Color.white;
+
+    public void SlotClick(int id)
     {
-        
+        _slotIDClicked = id;
+
+        // Loop through all slots
+        for (int i = 0; i < _slotList.transform.childCount; i++)
+        {
+            Button button = _slotList.transform.GetChild(i).GetChild(0).GetComponent<Button>();
+            if (i != _slotIDClicked)
+            {
+                // Set the default color for non-selected buttons
+                ColorBlock cb = button.colors;
+                cb.normalColor = _defaultColor;
+                cb.selectedColor = _defaultColor;
+                button.colors = cb;
+            }
+            else
+            {
+                // Set the selected color for the clicked button
+                ColorBlock cb = button.colors;
+                cb.normalColor = _selectedColor;
+                cb.selectedColor = _selectedColor;
+                button.colors = cb;
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void DeleteClick()
     {
-        
+        // TODO: Delete clicked slot
+    }
+
+    public void LoadClick()
+    {
+        // TODO: Load the game based on selected slot
+    }
+
+    public void BackClick()
+    {
+        gameObject.SetActive(false);
+        _mainMenuCanvas.SetActive(true);
     }
 }
