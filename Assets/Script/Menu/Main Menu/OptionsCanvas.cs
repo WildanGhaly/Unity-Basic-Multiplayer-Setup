bb1647d5e910d8ce3a6edb8028a1a85055094dbd
Hidayatullah Wildan Ghaly Buchary <13521015@std.stei.itb.ac.id>
2024-06-03 19:01:09 +0700

    feat: add optons canvas script and integrate with UI

--- Assets/Script/Menu/Main Menu/OptionsCanvas.cs
+++ Assets/Script/Menu/Main Menu/OptionsCanvas.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OptionsCanvas : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject _mainMenuCanvas;
+
+    [SerializeField] private Slider _volumeSlider;
+
+    private void Awake()
     {
-        
+        OnVolumeSliderChanged();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnVolumeSliderChanged()
     {
-        
+        AudioListener.volume = _volumeSlider.value;
+    }
+
+    public void BackClick()
+    {
+        gameObject.SetActive(false);
+        _mainMenuCanvas.SetActive(true);
     }
 }
