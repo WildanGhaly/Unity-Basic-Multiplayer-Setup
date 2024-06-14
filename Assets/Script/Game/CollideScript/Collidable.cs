316bef25f9561e612ec0850a826310531d18d4a1
Hidayatullah Wildan Ghaly Buchary <13521015@std.stei.itb.ac.id>
2024-06-14 16:15:15 +0700

    feat: setup collidable mechanism

---
+++ Assets/Script/Game/CollideScript/Collidable.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public abstract class Collidable : NetworkBehaviour
+{
+    public string promptMessage = "Collide";
+    public bool eventCollideEnter;
+    public bool eventCollideStay;
+    public bool eventCollideExit;
+    public void BaseCollideEnter()
+    {
+        if (eventCollideEnter)
+        {
+            GetComponent<CollisionEventEnter>().unityEvent.Invoke();
+        }
+        CollideEnter();
+    }
+
+    public void BaseCollideStay()
+    {
+        if (eventCollideStay)
+        {
+            GetComponent<CollisionEventStay>().unityEvent.Invoke();
+        }
+        CollideStay();
+    }
+
+    public void BaseCollideExit()
+    {
+        if (eventCollideExit)
+        {
+            GetComponent<CollisionEventExit>().unityEvent.Invoke();
+        }
+        CollideExit();
+    }
+
+    protected virtual void CollideEnter()
+    {
+
+    }
+
+    protected virtual void CollideStay()
+    {
+
+    }
+
+    protected virtual void CollideExit()
+    {
+
+    }
+}
