ad7a090184c5dc40b0cc535f99f42ab9c935953f
Hidayatullah Wildan Ghaly Buchary <13521015@std.stei.itb.ac.id>
2024-06-14 16:17:36 +0700

    chore: update collidable to use layer mask

--- Assets/Script/Game/CollideScript/Collidable.cs
+++ Assets/Script/Game/CollideScript/Collidable.cs
@@ -1,7 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using Mirror;
+using UnityEngine;
 
 public abstract class Collidable : NetworkBehaviour
 {
@@ -9,45 +7,76 @@ public abstract class Collidable : NetworkBehaviour
     public bool eventCollideEnter;
     public bool eventCollideStay;
     public bool eventCollideExit;
-    public void BaseCollideEnter()
+    public LayerMask collisionLayers;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (IsInCollisionLayer(collision.gameObject.layer))
+        {
+            BaseCollideEnter(collision);
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (IsInCollisionLayer(collision.gameObject.layer))
+        {
+            BaseCollideStay(collision);
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (IsInCollisionLayer(collision.gameObject.layer))
+        {
+            BaseCollideExit(collision);
+        }
+    }
+
+    private bool IsInCollisionLayer(int layer)
+    {
+        return (collisionLayers.value & (1 << layer)) != 0;
+    }
+
+    public void BaseCollideEnter(Collision collision)
     {
         if (eventCollideEnter)
         {
-            GetComponent<CollisionEventEnter>().unityEvent.Invoke();
+            GetComponent<CollisionEventEnter>()?.unityEvent.Invoke();
         }
-        CollideEnter();
+        CollideEnter(collision);
     }
 
-    public void BaseCollideStay()
+    public void BaseCollideStay(Collision collision)
     {
         if (eventCollideStay)
         {
-            GetComponent<CollisionEventStay>().unityEvent.Invoke();
+            GetComponent<CollisionEventStay>()?.unityEvent.Invoke();
         }
-        CollideStay();
+        CollideStay(collision);
     }
 
-    public void BaseCollideExit()
+    public void BaseCollideExit(Collision collision)
     {
         if (eventCollideExit)
         {
-            GetComponent<CollisionEventExit>().unityEvent.Invoke();
+            GetComponent<CollisionEventExit>()?.unityEvent.Invoke();
         }
-        CollideExit();
+        CollideExit(collision);
     }
 
-    protected virtual void CollideEnter()
+    protected virtual void CollideEnter(Collision collision)
     {
-
+        // Default behavior or leave empty for subclasses to override
     }
 
-    protected virtual void CollideStay()
+    protected virtual void CollideStay(Collision collision)
     {
-
+        // Default behavior or leave empty for subclasses to override
     }
 
-    protected virtual void CollideExit()
+    protected virtual void CollideExit(Collision collision)
     {
-
+        // Default behavior or leave empty for subclasses to override
     }
 }
