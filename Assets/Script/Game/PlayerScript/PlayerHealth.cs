8450d4814df2f42886de6ab57e03cd435fe0543d
Hidayatullah Wildan Ghaly Buchary <13521015@std.stei.itb.ac.id>
2024-06-14 09:05:56 +0700

    feat: add player attack and health setup

--- Assets/Script/Game/PlayerScript/PlayerHealth.cs
+++ Assets/Script/Game/PlayerScript/PlayerHealth.cs
@@ -1,18 +1,67 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using Mirror;
 
-public class PlayerHealth : MonoBehaviour
+public class PlayerHealth : NetworkBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SyncVar(hook = nameof(OnHealthChanged))]
+    private int currentHealth;
+
+    [SerializeField] private int maxHealth = 100;
+
+    public event System.Action<int, int> OnHealthChangedEvent;
+
+    public override void OnStartLocalPlayer()
     {
-        
+        if (isServer)
+        {
+            currentHealth = maxHealth;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void OnStartServer()
     {
-        
+        base.OnStartServer();
+        currentHealth = maxHealth;
+    }
+
+    [Server]
+    public void TakeDamage(int amount)
+    {
+        if (currentHealth <= 0) return;
+
+        currentHealth -= amount;
+        if (currentHealth <= 0)
+        {
+            currentHealth = 0;
+            // Add additional logic for player death if needed
+        }
+    }
+
+    [Server]
+    public void Heal(int amount)
+    {
+        if (currentHealth <= 0) return;
+
+        currentHealth += amount;
+        if (currentHealth > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+    }
+
+    private void OnHealthChanged(int oldHealth, int newHealth)
+    {
+        OnHealthChangedEvent?.Invoke(newHealth, maxHealth);
+        // Add additional logic for when health changes (e.g., updating UI)
+    }
+
+    public int GetCurrentHealth()
+    {
+        return currentHealth;
+    }
+
+    public int GetMaxHealth()
+    {
+        return maxHealth;
     }
 }
