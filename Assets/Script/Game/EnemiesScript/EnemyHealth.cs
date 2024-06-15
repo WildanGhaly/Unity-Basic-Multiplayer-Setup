6f7790988bef948b803fd82798aa26a40eb60902
Hidayatullah Wildan Ghaly Buchary <13521015@std.stei.itb.ac.id>
2024-06-15 16:45:56 +0700

    feat: add enemy health mechanism

---
+++ Assets/Script/Game/EnemiesScript/EnemyHealth.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : Health
+{
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+    protected override void OnHealthChanged(float oldHealth, float newHealth)
+    {
+        base.OnHealthChanged(oldHealth, newHealth);
+    }
+
+    protected override void RpcTakeDamage(float amount)
+    {
+        base.RpcTakeDamage(amount);
+    }
+
+    protected override void TakeDamage(float amount)
+    {
+        base.TakeDamage(amount);
+    }
+
+    protected override void DeathAnimation()
+    {
+        Debug.Log("Enemy Death Animation Play");
+    }
+
+    protected override void RpcTakeHeal(float amount)
+    {
+        base.RpcTakeHeal(amount);
+    }
+
+    protected override void TakeHeal(float amount)
+    {
+        base.TakeHeal(amount);
+    }
+}
